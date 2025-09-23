ef96f867b06e9062a6d5907b64df922e9783566c
kiarajarzinski <jarzinskikiara@gmail.com>
2025-09-23 14:51:27 -0300

    creacion de carpetas, servidor corriendo y base de datos conectada

---
+++ src/config/database.js
@@ -0,0 +1,18 @@
+//importaciones
+import { Sequelize } from "sequelize";
+import "dotenv/config";
+
+//creacion de la instancia sequelize 
+const sequelize = new Sequelize(
+process.env.DB_NAME,
+process.env.DB_USER,
+process.env.DB_PASSWORD,
+{
+host: process.env.DB_HOST,
+dialect: process.env.DB_DIALECT,
+
+logging: console.log,
+});
+
+//exportacion
+export default sequelize;
